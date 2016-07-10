ba46b95620a981b42175a879fcbd476e2964a3d7
Han Joosten <han.joosten.han@gmail.com>
2016-07-10 19:27:17 +0200

    WIP

--- src/Database/Design/Ampersand/Input/RDF/RDF.hs
+++ src/Database/Design/Ampersand/Input/RDF/RDF.hs
@@ -8,16 +8,17 @@ import Database.Design.Ampersand.Misc
 import Prelude hiding (putStrLn, writeFile) -- make sure everything is UTF8
 import Database.Design.Ampersand.Input.ADL1.CtxError
 import Database.Design.Ampersand.ADL1
-import Database.Design.Ampersand.Core.ParseTree
 import Data.RDF
+import Data.List
+import Data.Text as T (Text,unpack, pack)
 
 parseRdfFile :: Options 
               -> FilePath -> IO (Guarded [P_Context])
 parseRdfFile opts file = do 
-	result <- parseFile (XmlParser Nothing Nothing) file
-	case result of 
-	  Left (ParseFailure msg)  -> return $ mkRDFParseError file msg
-	  Right (rdf::TriplesList) -> return (pure [rdf2pContext file rdf])
+    result <- parseFile (XmlParser Nothing Nothing) file
+    case result of 
+      Left (ParseFailure msg)  -> return $ mkRDFParseError file msg
+      Right (rdf::TriplesList) -> return (pure [rdf2pContext file rdf])
 
 
 
@@ -26,11 +27,79 @@ parseRdfFile opts file = do
 timBernersLee :: IO ()
 timBernersLee = do
     Right (rdf::TriplesList) <- parseFile (XmlParser Nothing Nothing) "testdata.rdf"
-    let ts = query rdf (Just (UNode "http://www.w3.org/2011/Talks/0331-hyderabad-tbl/data#talk")) (Just (UNode "dct:title")) Nothing
-    let talks = map (\(Triple _ _ (LNode (PlainL s))) -> s) ts
-    print rdf
-    --print talks
-    --print $ baseUrl rdf
+    let members =  filter (tripleContainsNode memberNode) . triplesOf $ rdf
+    mapM_ print . take 200 . uniqueRelsOf $ rdf
+    mapM_ print members
+    putStrLn $ "#Triples: "++ (show . length . triplesOf $ rdf)
+    putStrLn $ "#Rels: "++ (show . length . uniqueRelsOf $ rdf)
+    putStrLn $ "#Members: "++ (show . length $ members)
+    mapM_ print . nub . objectsOfPredicate rdf $ memberNode
+     
+memberNode = UNode . T.pack $ "rdfs:member"
+uniqueRelsOf :: RDF a => a -> [TestRel]
+uniqueRelsOf rdf =  nub . map rdfRelType $ triplesOf rdf
+  where 
+    rdfRelType :: Triple -> TestRel
+    rdfRelType t 
+      = TestRel 
+         (node2String . predicateOf  $ t)  -- name
+         (sourceOf t)     -- source
+         (targetOf t)     -- target
+         isLit                          -- is it univalent? When the target is a literal, we assume that the relation is univalent.
+      where
+       sourceOf :: Triple -> TestCpt
+       sourceOf = cptOfNode . subjectOf
+       targetOf :: Triple -> TestCpt
+       targetOf = cptOfNode . objectOf
+
+       isLit :: Bool
+       isLit = case objectOf t of
+                    LNode _ -> True
+                    _ -> False
+       cptOfNode :: Node -> TestCpt
+       cptOfNode n = 
+         case n of 
+           LNode (PlainL _)     -> TestCpt litType
+           LNode (PlainLL _ _)  -> TestCpt litType
+           LNode (TypedL lit typ) -> fatal 45 $ "Interesting! `" ++T.unpack lit ++"` is of type `"++T.unpack typ++"`"
+           UNode x 
+             | isMember x -> findMember . objectOf  $ t
+             | otherwise  -> TestCpt $ "TODO: "++ show n 
+           _ -> TestCpt $ "xxx-"++node2String n
+        where 
+          litType = case predicateOf t of
+                      UNode x -> T.unpack x 
+                      _  -> fatal 54 "Unexpected node type."
+          isMember :: T.Text -> Bool
+          isMember txt =  txt == T.pack "rdfs:member"
+          
+            
+          findMember :: Node -> TestCpt
+          findMember = undefined
+node2String :: Node -> String 
+node2String n = 
+  case n of 
+    UNode t                -> "UNode: "++T.unpack t
+    BNode t                -> T.unpack t
+    BNodeGen i             -> show i
+    LNode (PlainL lit)     -> "Lit:"++T.unpack lit
+    LNode (PlainLL lit _)  -> "Lit:"++T.unpack lit
+    LNode (TypedL lit typ) -> fatal 45 $ "Interesting! `" ++T.unpack lit ++"` is of type `"++T.unpack typ++"`"
+
+data TestRel = TestRel String TestCpt TestCpt Bool -- Name, Source, Target, Univalent?
+  deriving Eq
+instance Show TestRel where
+  showsPrec _ (TestRel n s t pUni)
+   = showString . unlines $
+       [ "name:   "++n++if pUni then " [UNI]" else ""
+       , "source: "++show s
+       , "target: "++show t
+       ]
+data TestCpt = TestCpt String
+  deriving Eq
+instance Show TestCpt where
+  showsPrec _ (TestCpt x)
+   = showString x  
 
 rdf2pContext :: RDF a => String -> a -> P_Context
 rdf2pContext nm rdf  
